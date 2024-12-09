7610880c05f9f9511b416469ca0a62c456281cf0
ivansliepchenko <ivan.slepchenko@zettech.com>
2024-12-09 09:43:16 +0100

    feat: improve order filtering to include only active orders with NotStarted status for the current user

--- src/board/OrderInfo.tsx
+++ src/board/OrderInfo.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Box, Text, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, Grid, Table, Thead, Tbody, Tr, Th, Td, Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState, AppDispatch } from "../store/store";
-import { OrderStatus, updateStatus } from "../store/newOrderSlice";
 import { fetchProducts } from "../store/productsSlice";
 import { getRateTypeLabel, getRateUnitLabel } from "../newOrder/NewOrder";
 import { fetchOrderExecution } from "../store/executionSlice";
@@ -31,7 +30,7 @@ const OrderInfo: React.FC<OrderInfoProps> = ({ isOpen, onClose, orderId }) => {
         <Modal isOpen={isOpen} onClose={onClose}>
             <ModalOverlay />
             <ModalContent minWidth="1200px">
-                <ModalHeader>Order Information</ModalHeader>
+                <ModalHeader>Your Order</ModalHeader>
                 <ModalCloseButton />
                 <ModalBody>
                     <Tabs>
@@ -45,7 +44,7 @@ const OrderInfo: React.FC<OrderInfoProps> = ({ isOpen, onClose, orderId }) => {
                                     <Grid templateColumns="repeat(2, 1fr)" gap="4" mb="4">
                                         <Box border="1px" borderColor="gray.200" p="4" borderRadius="md">
                                             <Text fontSize="md" fontWeight="bold" mb="2">Order Information</Text>
-                                            <Grid templateColumns="repeat(2, 1fr)" gap="1" mb="4" borderBottom="1px" borderColor="gray.200">
+                                            <Grid templateColumns="repeat(2, 1fr)" gap="1" borderColor="gray.200">
                                                 <Box>
                                                     <Text fontSize="xs">Recipe creation date:</Text>
                                                     <Text>{order.recipeDate}</Text>
@@ -90,7 +89,7 @@ const OrderInfo: React.FC<OrderInfoProps> = ({ isOpen, onClose, orderId }) => {
                                         </Box>
                                         <Box border="1px" borderColor="gray.200" p="4" borderRadius="md">
                                             <Text fontSize="md" fontWeight="bold" mb="2">Recipe Information</Text>
-                                            <Grid templateColumns="repeat(2, 1fr)" gap="1" mb="4" borderBottom="1px" borderColor="gray.200">
+                                            <Grid templateColumns="repeat(2, 1fr)" gap="1" borderColor="gray.200">
                                                 <Box>
                                                     <Text fontSize="xs">Total Compounds Density:</Text>
                                                     <Text>{order.orderRecipe.totalCompoundsDensity.toFixed(2)}</Text>
@@ -131,47 +130,48 @@ const OrderInfo: React.FC<OrderInfoProps> = ({ isOpen, onClose, orderId }) => {
                                         </Box>
                                     </Grid>
                                     <Text fontSize="md" fontWeight="bold" mt="4" mb="2">Product Details</Text>
-                                    <Box maxHeight="300px" overflowY="auto" bg="gray.50" p="2" borderRadius="md">
+                                    <Box maxHeight="300px" overflowY="auto" bg="gray.50" borderRadius="md">
                                         <Table variant="simple" size="sm" w="full">
                                             <Thead bg="orange.100">
                                                 <Tr>
-                                                    <Th rowSpan={2} width="35%" whiteSpace="nowrap" borderBottom="1px" borderColor="gray.200">Product Name</Th>
-                                                    <Th rowSpan={2} whiteSpace="nowrap" borderBottom="1px" borderColor="gray.200">Density (g/ml)</Th>
-                                                    <Th rowSpan={2} whiteSpace="nowrap" borderBottom="1px" borderColor="gray.200">Rate</Th>
-                                                    <Th rowSpan={2} whiteSpace="nowrap" borderBottom="1px" borderColor="gray.200">Rate Unit</Th>
-                                                    <Th colSpan={2} whiteSpace="nowrap" borderBottom="1px" borderColor="gray.200">Slurry Total</Th>
-                                                    <Th colSpan={2} whiteSpace="nowrap" borderBottom="1px" borderColor="gray.200">Rate</Th>
-                                                    <Th colSpan={2} whiteSpace="nowrap" borderBottom="1px" borderColor="gray.200">Rate</Th>
-                                                    <Th colSpan={2} whiteSpace="nowrap" borderBottom="1px" borderColor="gray.200">Slurry</Th>
+                                                    <Th rowSpan={2} width="35%" whiteSpace="nowrap" borderBottom="1px" borderColor="gray.400">Product Name</Th>
+                                                    <Th rowSpan={1} whiteSpace="nowrap" borderLeft="1px" borderBottom="1px" borderColor="gray.400">Density</Th>
+                                                    <Th rowSpan={2} whiteSpace="nowrap" borderLeft="1px" borderBottom="1px" borderColor="gray.400">Rate</Th>
+                                                    <Th rowSpan={2} whiteSpace="nowrap" borderLeft="1px" borderBottom="1px" borderColor="gray.400">Rate Unit</Th>
+                                                    <Th colSpan={2} whiteSpace="nowrap" borderLeft="1px" borderBottom="1px" borderColor="gray.400">Slurry Total</Th>
+                                                    <Th colSpan={2} whiteSpace="nowrap" borderLeft="1px" borderBottom="1px" borderColor="gray.400">Rate</Th>
+                                                    <Th colSpan={2} whiteSpace="nowrap" borderLeft="1px" borderBottom="1px" borderColor="gray.400">Rate</Th>
+                                                    <Th colSpan={2} whiteSpace="nowrap" borderLeft="1px" borderBottom="1px" borderColor="gray.400">Slurry</Th>
                                                 </Tr>
                                                 <Tr>
-                                                    <Th whiteSpace="nowrap" borderBottom="1px" borderColor="gray.200">ml/U/KS</Th>
-                                                    <Th whiteSpace="nowrap" borderBottom="1px" borderColor="gray.200">g/U/KS</Th>
-                                                    <Th whiteSpace="nowrap" borderBottom="1px" borderColor="gray.200">ml/U/KS</Th>
-                                                    <Th whiteSpace="nowrap" borderBottom="1px" borderColor="gray.200">g/U/KS</Th>
-                                                    <Th whiteSpace="nowrap" borderBottom="1px" borderColor="gray.200">ml/100kg</Th>
-                                                    <Th whiteSpace="nowrap" borderBottom="1px" borderColor="gray.200">g/100kg</Th>
-                                                    <Th whiteSpace="nowrap" borderBottom="1px" borderColor="gray.200">ml Recipe/Mix</Th>
-                                                    <Th whiteSpace="nowrap" borderBottom="1px" borderColor="gray.200">kg Recipe/Weight</Th>
+                                                    <Th whiteSpace="nowrap" borderBottom="1px" borderLeft="1px" borderColor="gray.400">g/ml</Th>
+                                                    <Th whiteSpace="nowrap" borderBottom="1px" borderLeft="1px" borderColor="gray.400">ml/U/KS</Th>
+                                                    <Th whiteSpace="nowrap" borderBottom="1px" borderLeft="1px" borderColor="gray.400">g/U/KS</Th>
+                                                    <Th whiteSpace="nowrap" borderBottom="1px" borderLeft="1px" borderColor="gray.400">ml/U/KS</Th>
+                                                    <Th whiteSpace="nowrap" borderBottom="1px" borderLeft="1px" borderColor="gray.400">g/U/KS</Th>
+                                                    <Th whiteSpace="nowrap" borderBottom="1px" borderLeft="1px" borderColor="gray.400">ml/100kg</Th>
+                                                    <Th whiteSpace="nowrap" borderBottom="1px" borderLeft="1px" borderColor="gray.400">g/100kg</Th>
+                                                    <Th whiteSpace="nowrap" borderBottom="1px" borderLeft="1px" borderColor="gray.400">liters</Th>
+                                                    <Th whiteSpace="nowrap" borderBottom="1px" borderLeft="1px" borderColor="gray.400">kg</Th>
                                                 </Tr>
                                             </Thead>
                                             <Tbody>
                                                 {order.productDetails && [...order.productDetails]
                                                     .sort((a, b) => a.index - b.index) // Sort by index
                                                     .map((productDetail, index) => (
-                                                        <Tr key={index} borderBottom="1px" borderColor="gray.200">
-                                                            <Td width="35%" borderBottom="1px" borderColor="gray.200">{productDetail.product ? productDetail.product.name : 'undefined'}</Td>
-                                                            <Td borderBottom="1px" borderColor="gray.200">{productDetail.product?.density.toFixed(2)}</Td>
-                                                            <Td borderBottom="1px" borderColor="gray.200">{productDetail.rate.toFixed(2)}</Td>
-                                                            <Td borderBottom="1px" borderColor="gray.200">{`${getRateTypeLabel(productDetail.rateType)}/${getRateUnitLabel(productDetail.rateUnit)}`}</Td>
-                                                            <Td borderBottom="1px" borderColor="gray.200">{order.orderRecipe.productRecipes[index]?.kgSlurryRecipeToMix.toFixed(2)}</Td>
-                                                            <Td borderBottom="1px" borderColor="gray.200">{order.orderRecipe.productRecipes[index]?.literSlurryRecipeToMix.toFixed(2)}</Td>
-                                                            <Td borderBottom="1px" borderColor="gray.200">{order.orderRecipe.productRecipes[index]?.rateMltoU_KS.toFixed(2)}</Td>
-                                                            <Td borderBottom="1px" borderColor="gray.200">{order.orderRecipe.productRecipes[index]?.rateGToU_KS.toFixed(2)}</Td>
-                                                            <Td borderBottom="1px" borderColor="gray.200">{order.orderRecipe.productRecipes[index]?.rateMlTo100Kg.toFixed(2)}</Td>
-                                                            <Td borderBottom="1px" borderColor="gray.200">{order.orderRecipe.productRecipes[index]?.rateGTo100Kg.toFixed(2)}</Td>
-                                                            <Td borderBottom="1px" borderColor="gray.200">{order.orderRecipe.productRecipes[index]?.literSlurryRecipeToMix.toFixed(2)}</Td>
-                                                            <Td borderBottom="1px" borderColor="gray.200">{order.orderRecipe.productRecipes[index]?.kgSlurryRecipeToMix.toFixed(2)}</Td>
+                                                        <Tr key={index} borderBottom="1px" borderColor="gray.400">
+                                                            <Td width="35%" borderBottom="1px" borderColor="gray.400">{productDetail.product ? productDetail.product.name : 'undefined'}</Td>
+                                                            <Td borderBottom="1px" borderColor="gray.400">{productDetail.product?.density.toFixed(2)}</Td>
+                                                            <Td borderBottom="1px" borderColor="gray.400">{productDetail.rate.toFixed(2)}</Td>
+                                                            <Td borderBottom="1px" borderColor="gray.400">{`${getRateTypeLabel(productDetail.rateType)}/${getRateUnitLabel(productDetail.rateUnit)}`}</Td>
+                                                            <Td borderBottom="1px" borderColor="gray.400">{order.orderRecipe.productRecipes[index]?.kgSlurryRecipeToMix.toFixed(2)}</Td>
+                                                            <Td borderBottom="1px" borderColor="gray.400">{order.orderRecipe.productRecipes[index]?.literSlurryRecipeToMix.toFixed(2)}</Td>
+                                                            <Td borderBottom="1px" borderColor="gray.400">{order.orderRecipe.productRecipes[index]?.rateMltoU_KS.toFixed(2)}</Td>
+                                                            <Td borderBottom="1px" borderColor="gray.400">{order.orderRecipe.productRecipes[index]?.rateGToU_KS.toFixed(2)}</Td>
+                                                            <Td borderBottom="1px" borderColor="gray.400">{order.orderRecipe.productRecipes[index]?.rateMlTo100Kg.toFixed(2)}</Td>
+                                                            <Td borderBottom="1px" borderColor="gray.400">{order.orderRecipe.productRecipes[index]?.rateGTo100Kg.toFixed(2)}</Td>
+                                                            <Td borderBottom="1px" borderColor="gray.400">{order.orderRecipe.productRecipes[index]?.literSlurryRecipeToMix.toFixed(2)}</Td>
+                                                            <Td borderBottom="1px" borderColor="gray.400">{order.orderRecipe.productRecipes[index]?.kgSlurryRecipeToMix.toFixed(2)}</Td>
                                                         </Tr>
                                                     ))}
                                             </Tbody>
@@ -195,9 +195,9 @@ const OrderInfo: React.FC<OrderInfoProps> = ({ isOpen, onClose, orderId }) => {
                                         <Table variant="simple" size="sm" w="full">
                                             <Thead bg="orange.100">
                                                 <Tr>
-                                                    <Th width="35%" whiteSpace="nowrap" borderBottom="1px" borderColor="gray.200">Product Name</Th>
-                                                    <Th whiteSpace="nowrap" borderBottom="1px" borderColor="gray.200">Application Photo</Th>
-                                                    <Th whiteSpace="nowrap" borderBottom="1px" borderColor="gray.200">Consumption Photo</Th>
+                                                    <Th width="35%" whiteSpace="nowrap" borderBottom="2px" borderColor="gray.400">Product Name</Th>
+                                                    <Th whiteSpace="nowrap" borderBottom="2px" borderColor="gray.400">Application Photo</Th>
+                                                    <Th whiteSpace="nowrap" borderBottom="2px" borderColor="gray.400">Consumption Photo</Th>
                                                 </Tr>
                                             </Thead>
                                             <Tbody>
@@ -206,10 +206,10 @@ const OrderInfo: React.FC<OrderInfoProps> = ({ isOpen, onClose, orderId }) => {
                                                     .map((productDetail, index) => {
                                                         const productExecution = orderExecution?.productExecutions.find(pe => pe.productId === productDetail.product?.id);
                                                         return (
-                                                            <Tr key={index} borderBottom="1px" borderColor="gray.200">
-                                                                <Td width="35%" borderBottom="1px" borderColor="gray.200">{productDetail.product ? productDetail.product.name : 'undefined'}</Td>
-                                                                <Td borderBottom="1px" borderColor="gray.200">{productExecution?.applicationPhoto ? <img src={productExecution.applicationPhoto} alt="Application" /> : 'No Photo'}</Td>
-                                                                <Td borderBottom="1px" borderColor="gray.200">{productExecution?.consumptionPhoto ? <img src={productExecution.consumptionPhoto} alt="Consumption" /> : 'No Photo'}</Td>
+                                                            <Tr key={index} borderBottom="2px" borderColor="gray.400">
+                                                                <Td width="35%" borderBottom="2px" borderColor="gray.400">{productDetail.product ? productDetail.product.name : 'undefined'}</Td>
+                                                                <Td borderBottom="2px" borderColor="gray.400">{productExecution?.applicationPhoto ? <img src={productExecution.applicationPhoto} alt="Application" /> : 'No Photo'}</Td>
+                                                                <Td borderBottom="2px" borderColor="gray.400">{productExecution?.consumptionPhoto ? <img src={productExecution.consumptionPhoto} alt="Consumption" /> : 'No Photo'}</Td>
                                                             </Tr>
                                                         );
                                                     })}
