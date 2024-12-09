7cc42cc4a3ee2a36f50df2c1a7afccce5729c8ce
ivansliepchenko <ivan.slepchenko@zettech.com>
2024-12-09 11:05:43 +0100

    feat: enhance OrderExecutionTab to display additional photo details and improve layout for better user experience

--- src/board/orderInfo/OrderExecutionTab.tsx
+++ src/board/orderInfo/OrderExecutionTab.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Box, Text, Table, Thead, Tbody, Tr, Th, Td, Image, Modal, ModalOverlay, ModalContent, ModalBody } from "@chakra-ui/react";
+import { Box, Text, Table, Thead, Tbody, Tr, Th, Td, Image, Modal, ModalOverlay, ModalContent, ModalBody, HStack } from "@chakra-ui/react";
 import { Order } from "../../store/newOrderSlice";
 import { OrderExecution } from "../../store/executionSlice";
 
@@ -17,36 +17,45 @@ const OrderExecutionTab: React.FC<{ order: Order, orderExecution: OrderExecution
     return (
         <Box w="full">
             <Text fontSize="md" fontWeight="bold" mt="4" mb="2">Order Execution Photos</Text>
-            <Box>
-                <Text fontSize="xs">Packing Photo:</Text>
-                {orderExecution?.packingPhoto ? (
-                    <Image
-                        src={orderExecution.packingPhoto}
-                        alt="Packing"
-                        width="150px"
-                        height="100px"
-                        objectFit="cover"
-                        onClick={() => handlePhotoClick(orderExecution.packingPhoto)}
-                        cursor="pointer"
-                        title="Expected Packing Photo"
-                    />
-                ) : 'No Photo'}
-            </Box>
-            <Box>
-                <Text fontSize="xs">Consumption Photo:</Text>
+            <HStack spacing="4">
+
+                <Box>
+                    <Text fontSize="lg">Packing Photo:</Text>
+                    {orderExecution?.packingPhoto ? (
+                        <>
+                            <Text fontSize="xs" fontWeight="bold">Expected quantity: {orderExecution.packedQuantity} Units</Text>
+                            <Image
+                                src={orderExecution.packingPhoto}
+                                alt="Packing"
+                                width="150px"
+                                height="100px"
+                                objectFit="cover"
+                                onClick={() => handlePhotoClick(orderExecution.packingPhoto)}
+                                cursor="pointer"
+                                title="Expected Packing Photo"
+                            />
+                        </>
+                    ) : 'No Photo'}
+                </Box>
+                
                 {orderExecution?.consumptionPhoto ? (
-                    <Image
-                        src={orderExecution.consumptionPhoto}
-                        alt="Consumption"
-                        width="150px"
-                        height="100px"
-                        objectFit="cover"
-                        onClick={() => handlePhotoClick(orderExecution.consumptionPhoto)}
-                        cursor="pointer"
-                        title="Expected Consumption Photo"
-                    />
+                    <Box>
+                        <Text fontSize="lg">Consumption Photo:</Text>
+                        <Text fontSize="xs" fontWeight="bold">Consumption Info: {order?.orderRecipe?.slurryTotalKgRecipeToWeight}</Text>
+                        <Image
+                            src={orderExecution.consumptionPhoto}
+                            alt="Consumption"
+                            width="150px"
+                            height="100px"
+                            objectFit="cover"
+                            onClick={() => handlePhotoClick(orderExecution.consumptionPhoto)}
+                            cursor="pointer"
+                            title="Expected Consumption Photo"
+                        />
+                    </Box>
                 ) : 'No Photo'}
-            </Box>
+                
+            </HStack>
             <Text fontSize="md" fontWeight="bold" mt="4" mb="2">Product Execution Details</Text>
             <Box maxHeight="300px" overflowY="auto" bg="gray.50" p="2" borderRadius="md">
                 <Table variant="simple" size="sm" w="full">
@@ -62,35 +71,42 @@ const OrderExecutionTab: React.FC<{ order: Order, orderExecution: OrderExecution
                             .sort((a, b) => a.index - b.index) // Sort by index
                             .map((productDetail, index) => {
                                 const productExecution = orderExecution?.productExecutions.find(pe => pe.productId === productDetail.product?.id);
+                                const productRecipe = order.orderRecipe.productRecipes.find(pr => pr.productDetail.product?.id === productDetail.product?.id);
                                 return (
                                     <Tr key={index} borderBottom="2px" borderColor="gray.400">
                                         <Td width="35%" borderBottom="2px" borderColor="gray.400">{productDetail.product ? productDetail.product.name : 'undefined'}</Td>
                                         <Td borderBottom="2px" borderColor="gray.400">
                                             {productExecution?.applicationPhoto ? (
-                                                <Image
-                                                    src={productExecution.applicationPhoto}
-                                                    alt="Application"
-                                                    width="150px"
-                                                    height="100px"
-                                                    objectFit="cover"
-                                                    onClick={() => handlePhotoClick(productExecution.applicationPhoto)}
-                                                    cursor="pointer"
-                                                    title="Expected Application Photo"
-                                                />
+                                                <>
+                                                    <Text fontSize="xs" fontWeight="bold">Expected: {productRecipe?.rateGTo100Kg.toFixed(2)} g</Text>
+                                                    <Image
+                                                        src={productExecution.applicationPhoto}
+                                                        alt="Application"
+                                                        width="150px"
+                                                        height="100px"
+                                                        objectFit="cover"
+                                                        onClick={() => handlePhotoClick(productExecution.applicationPhoto)}
+                                                        cursor="pointer"
+                                                        title="Expected Application Photo"
+                                                    />
+                                                </>
                                             ) : 'No Photo'}
                                         </Td>
                                         <Td borderBottom="2px" borderColor="gray.400">
                                             {productExecution?.consumptionPhoto ? (
-                                                <Image
-                                                    src={productExecution.consumptionPhoto}
-                                                    alt="Consumption"
-                                                    width="150px"
-                                                    height="100px"
-                                                    objectFit="cover"
-                                                    onClick={() => handlePhotoClick(productExecution.consumptionPhoto)}
-                                                    cursor="pointer"
-                                                    title="Expected Consumption Photo"
-                                                />
+                                                <>
+                                                    <Text fontSize="xs" fontWeight="bold">Expected: {productRecipe?.kgSlurryRecipeToMix.toFixed(2)} kg</Text>
+                                                    <Image
+                                                        src={productExecution.consumptionPhoto}
+                                                        alt="Consumption"
+                                                        width="150px"
+                                                        height="100px"
+                                                        objectFit="cover"
+                                                        onClick={() => handlePhotoClick(productExecution.consumptionPhoto)}
+                                                        cursor="pointer"
+                                                        title="Expected Consumption Photo"
+                                                    />
+                                                </>
                                             ) : 'No Photo'}
                                         </Td>
                                     </Tr>
