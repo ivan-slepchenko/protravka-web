d7eae639ebf47e588565f90bd2cd6e4d98940dfc
ivansliepchenko <ivan.slepchenko@zettech.com>
2024-12-09 11:42:15 +0100

    feat: adjust OrderExecutionTab width based on application method for improved layout

--- src/board/orderInfo/OrderExecutionTab.tsx
+++ src/board/orderInfo/OrderExecutionTab.tsx
@@ -19,7 +19,7 @@ const OrderExecutionTab: React.FC<{ order: Order, orderExecution: OrderExecution
     return (
         <Box w="full">
             <Text fontSize="md" fontWeight="bold" mt="4" mb="2">Order Execution Photos:</Text>
-            <Box overflowY="auto" bg="gray.50" borderRadius="md">
+            <Box overflowY="auto" bg="gray.50" borderRadius="md" w={applicationMethod !== 'Surry' ? "50%" : "full"}>
                 <Table variant="simple" size="sm" w="full">
                     <Thead bg="orange.100">
                         <Tr>
