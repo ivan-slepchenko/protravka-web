ce89462b68de1e6cedc5424846d4f4b8ef8136a9
ivansliepchenko <ivan.slepchenko@zettech.com>
2024-12-09 11:39:43 +0100

    feat: enhance OrderExecutionTab with a responsive table layout for photos and details

--- src/board/orderInfo/OrderExecutionTab.tsx
+++ src/board/orderInfo/OrderExecutionTab.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
-import { Box, Text, Table, Thead, Tbody, Tr, Th, Td, Image, Modal, ModalOverlay, ModalContent, ModalBody, HStack } from "@chakra-ui/react";
+import { Box, Text, Table, Thead, Tbody, Tr, Th, Td, Image, Modal, ModalOverlay, ModalContent, ModalBody, HStack, ModalCloseButton } from "@chakra-ui/react";
 import { Order } from "../../store/newOrderSlice";
 import { OrderExecution } from "../../store/executionSlice";
 
 const OrderExecutionTab: React.FC<{ order: Order, orderExecution: OrderExecution }> = ({ order, orderExecution }) => {
     const [selectedPhoto, setSelectedPhoto] = useState<string | null | undefined>(null);
+
+    const applicationMethod = orderExecution?.applicationMethod;
 
     const handlePhotoClick = (photoUrl: string | null | undefined) => {
         setSelectedPhoto(photoUrl);
@@ -16,46 +18,65 @@ const OrderExecutionTab: React.FC<{ order: Order, orderExecution: OrderExecution
 
     return (
         <Box w="full">
-            <Text fontSize="md" fontWeight="bold" mt="4" mb="2">Order Execution Photos</Text>
-            <HStack spacing="4">
-
-                <Box>
-                    <Text fontSize="lg">Packing Photo:</Text>
-                    {orderExecution?.packingPhoto ? (
-                        <>
-                            <Text fontSize="xs" fontWeight="bold">Expected quantity: {orderExecution.packedQuantity} Units</Text>
-                            <Image
-                                src={orderExecution.packingPhoto}
-                                alt="Packing"
-                                width="150px"
-                                height="100px"
-                                objectFit="cover"
-                                onClick={() => handlePhotoClick(orderExecution.packingPhoto)}
-                                cursor="pointer"
-                                title="Expected Packing Photo"
-                            />
-                        </>
-                    ) : 'No Photo'}
-                </Box>
-                
-                {orderExecution?.consumptionPhoto ? (
-                    <Box>
-                        <Text fontSize="lg">Consumption Photo:</Text>
-                        <Text fontSize="xs" fontWeight="bold">Consumption Info: {order?.orderRecipe?.slurryTotalKgRecipeToWeight}</Text>
-                        <Image
-                            src={orderExecution.consumptionPhoto}
-                            alt="Consumption"
-                            width="150px"
-                            height="100px"
-                            objectFit="cover"
-                            onClick={() => handlePhotoClick(orderExecution.consumptionPhoto)}
-                            cursor="pointer"
-                            title="Expected Consumption Photo"
-                        />
-                    </Box>
-                ) : 'No Photo'}
-                
-            </HStack>
+            <Text fontSize="md" fontWeight="bold" mt="4" mb="2">Order Execution Photos:</Text>
+            <Box overflowY="auto" bg="gray.50" borderRadius="md">
+                <Table variant="simple" size="sm" w="full">
+                    <Thead bg="orange.100">
+                        <Tr>
+                            <Th borderLeft="1px" whiteSpace="nowrap" borderBottom="1px" borderColor="gray.400">Packing Photo</Th>
+                            <Th borderLeft="1px" whiteSpace="nowrap" borderBottom="1px" borderColor="gray.400">Expected Quantity</Th>
+                            {applicationMethod === 'Surry' && (
+                                <>
+                                    <Th borderLeft="1px" whiteSpace="nowrap" borderBottom="1px" borderColor="gray.400">Consumption Photo</Th>
+                                    <Th borderLeft="1px" whiteSpace="nowrap" borderBottom="1px" borderColor="gray.400">Consumption Info</Th>
+                                </>
+                            )}
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        <Tr borderBottom="1px" borderColor="gray.400">
+                            <Td borderBottom="1px" borderColor="gray.400">
+                                {orderExecution?.packingPhoto ? (
+                                    <Image
+                                        src={orderExecution.packingPhoto}
+                                        alt="Packing"
+                                        width="150px"
+                                        height="100px"
+                                        objectFit="cover"
+                                        onClick={() => handlePhotoClick(orderExecution.packingPhoto)}
+                                        cursor="pointer"
+                                        title="Expected Packing Photo"
+                                    />
+                                ) : 'No Photo'}
+                            </Td>
+                            <Td borderBottom="1px" borderColor="gray.400">
+                                <Text fontSize="xs" fontWeight="bold">{orderExecution?.packedQuantity} Units</Text>
+                            </Td>
+                            {applicationMethod === 'Surry' && (
+                                <>
+                                    <Td borderBottom="1px" borderColor="gray.400">
+                                        {orderExecution?.consumptionPhoto ? (
+                                            <Image
+                                                src={orderExecution.consumptionPhoto}
+                                                alt="Consumption"
+                                                width="150px"
+                                                height="100px"
+                                                objectFit="cover"
+                                                onClick={() => handlePhotoClick(orderExecution.consumptionPhoto)}
+                                                cursor="pointer"
+                                                title="Expected Consumption Photo"
+                                            />
+                                        ) : 'No Photo'}
+                                    </Td>
+                                    <Td borderBottom="1px" borderColor="gray.400">
+                                        <Text fontSize="xs" fontWeight="bold">{order?.orderRecipe?.slurryTotalKgRecipeToWeight}</Text>
+                                    </Td>
+                                </>
+                            )}
+                        </Tr>
+                    </Tbody>
+                </Table>
+            </Box>
             <Text fontSize="md" fontWeight="bold" mt="4" mb="2">Product Execution Details</Text>
             <Box overflowY="auto" bg="gray.50" borderRadius="md">
                 <Table variant="simple" size="sm" w="full">
@@ -119,7 +140,8 @@ const OrderExecutionTab: React.FC<{ order: Order, orderExecution: OrderExecution
             </Box>
             <Modal isOpen={!!selectedPhoto} onClose={handleClose}>
                 <ModalOverlay />
-                <ModalContent width="800px" height="600px" maxWidth="unset">
+                <ModalContent width="800px" height="600px" maxWidth="unset" p="6">
+                    <ModalCloseButton />
                     <ModalBody>
                         {selectedPhoto && (
                             <Image
