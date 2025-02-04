511dacef018c61706a057b3ea5641e8fb8a73bf7
Ivan Sliepchenko <ivan.slepchenko@zettech.com>
2025-02-04 14:15:26 +0100

    Update viewport meta tag for portrait orientation; enhance TKW details modal layout and scrolling behavior

--- src/board/TkwDetailsModal.tsx
+++ src/board/TkwDetailsModal.tsx
@@ -29,50 +29,48 @@ const TkwDetailsModal: React.FC<TkwDetailsModalProps> = ({ onClose, order, measu
             <ModalContent h="full">
                 <ModalHeader fontSize={{ base: "sm", md: "lg" }}>TKW Details</ModalHeader>
                 <ModalCloseButton />
-                <ModalBody h="full">
-                    <Center w="full" h="full">
-                        <VStack spacing={4} align="start" w="full">
-                            <Badge autoCapitalize='none' w="full" colorScheme="gray">
-                                <Text fontSize={{ base: "md", md: "lg" }}>
+                <ModalBody h="full" overflow='auto'>
+                    <VStack spacing={4} align="start" w="full" overflow='auto' mt="auto" mb="auto">
+                        <Badge autoCapitalize='none' w="full" colorScheme="gray">
+                            <Text fontSize={{ base: "md", md: "lg" }}>
                                     Raw Average TKW: {order.tkw.toFixed(2)} gr.
-                                </Text>
-                            </Badge>
-                            <Grid templateColumns="repeat(2, 1fr)" gap={4} w="full">
-                                <GridItem px={1}>
-                                    {order.tkwProbesPhoto && <ImageWithModal src={order.tkwProbesPhoto} fullSize />}
-                                </GridItem>
-                                <GridItem px={1}>
-                                    <VStack spacing={4} alignItems={'start'}>
-                                        <Text>Probe 1: {order.tkwRep1} gr.</Text>
-                                        <Text>Probe 2: {order.tkwRep2} gr.</Text>
-                                        <Text>Probe 3: {order.tkwRep3} gr.</Text>
-                                    </VStack>
-                                </GridItem>
-                            </Grid>
+                            </Text>
+                        </Badge>
+                        <Grid templateColumns="repeat(2, 1fr)" gap={4} w="full">
+                            <GridItem px={1}>
+                                {order.tkwProbesPhoto && <ImageWithModal src={order.tkwProbesPhoto} fullSize />}
+                            </GridItem>
+                            <GridItem px={1}>
+                                <VStack spacing={4} alignItems={'start'}>
+                                    <Text>Probe 1: {order.tkwRep1} gr.</Text>
+                                    <Text>Probe 2: {order.tkwRep2} gr.</Text>
+                                    <Text>Probe 3: {order.tkwRep3} gr.</Text>
+                                </VStack>
+                            </GridItem>
+                        </Grid>
 
-                            {measurements.length > 0 && 
-                                <>
-                                    <Text fontWeight="bold">Treated TKW Measurements:</Text>
-                                    {measurements.map((measurement, index) => (
-                                        <Grid templateColumns="repeat(2, 1fr)" gap={4} w="full" key={index} borderWidth={1} borderRadius="md" p={2}>
-                                            <GridItem>
-                                                {measurement.tkwProbesPhoto && <ImageWithModal src={measurement.tkwProbesPhoto} fullSize />}
-                                            </GridItem>
-                                            <GridItem>
-                                                <VStack spacing={4} justify="space-around">
-                                                    <Text>Measurement {index + 1}:</Text>
-                                                    <Text>Probe 1: {measurement.tkwProbe1} gr.</Text>
-                                                    <Text>Probe 2: {measurement.tkwProbe2} gr.</Text>
-                                                    <Text>Probe 3: {measurement.tkwProbe3} gr.</Text>
-                                                    <Text>Average TKW: {calculateAverageTkw([measurement])} gr.</Text>
-                                                </VStack>
-                                            </GridItem>
-                                        </Grid>
-                                    ))}
-                                </>
-                            }  
-                        </VStack>
-                    </Center>
+                        {measurements.length > 0 && 
+                            <>
+                                <Text fontWeight="bold">Treated TKW Measurements:</Text>
+                                {measurements.map((measurement, index) => (
+                                    <Grid templateColumns="repeat(2, 1fr)" gap={4} w="full" key={index} borderWidth={1} borderRadius="md" p={2}>
+                                        <GridItem>
+                                            {measurement.tkwProbesPhoto && <ImageWithModal src={measurement.tkwProbesPhoto} fullSize />}
+                                        </GridItem>
+                                        <GridItem>
+                                            <VStack spacing={4} justify="space-around">
+                                                <Text>Measurement {index + 1}:</Text>
+                                                <Text>Probe 1: {measurement.tkwProbe1} gr.</Text>
+                                                <Text>Probe 2: {measurement.tkwProbe2} gr.</Text>
+                                                <Text>Probe 3: {measurement.tkwProbe3} gr.</Text>
+                                                <Text>Average TKW: {calculateAverageTkw([measurement])} gr.</Text>
+                                            </VStack>
+                                        </GridItem>
+                                    </Grid>
+                                ))}
+                            </>
+                        }  
+                    </VStack>
                 </ModalBody>
                 <ModalFooter>
                     <Button variant="ghost" onClick={onClose}>Close</Button>
