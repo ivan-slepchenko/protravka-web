15ac561c150b2879ec3a830cee19c0314535de1b
Ivan Sliepchenko <ivan.slepchenko@zettech.com>
2025-02-09 12:13:37 +0100

    Add number input type and step attribute to applied rate input field

--- src/execution/OrderExecution3ApplyingProduct.tsx
+++ src/execution/OrderExecution3ApplyingProduct.tsx
@@ -75,7 +75,9 @@ const OrderExecution3ApplyingProduct = () => {
                     <Input
                         placeholder="Enter value"
                         value={(currentProductExecution && currentProductExecution.appliedRateKg !== undefined) ? currentProductExecution.appliedRateKg : ''}
-                        onChange={(e) => handleValueChange(currentProductId, e.target.value === undefined ? undefined : parseFloat(e.target.value))}
+                        onChange={(e) => handleValueChange(currentProductId, e.target.value === '' ? undefined : parseFloat(e.target.value.endsWith('.') ? e.target.value.slice(0, -1) : e.target.value))}
+                        type="number"
+                        step="0.01"
                         borderColor={inputError ? 'red.500' : 'gray.200'}
                     />
                 </Td>
