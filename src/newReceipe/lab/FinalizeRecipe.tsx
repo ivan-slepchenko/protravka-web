9c269e2c6fb68a74f2c6b9dca3bfc677a476fbdb
Ivan Sliepchenko <ivan.slepchenko@zettech.com>
2025-03-02 18:46:40 +0100

    Refactor FinalizeRecipe component; simplify tkwMeasurementIntervals initialization by removing conditional logic

--- src/newReceipe/lab/FinalizeRecipe.tsx
+++ src/newReceipe/lab/FinalizeRecipe.tsx
@@ -208,10 +208,10 @@ export const FinalizeRecipe = () => {
     const operatorRole = Role.OPERATOR;
     const filteredOperators = operators.filter(operator => operator.roles.includes(operatorRole));
 
-    const tkwMeasurementIntervals = [60, 45, 30, 20, 10];
-    if (import.meta.env.MODE === 'development') {
-        tkwMeasurementIntervals.push(1);
-    }
+    const tkwMeasurementIntervals = [60, 45, 30, 20, 10, 1];
+    // if (import.meta.env.MODE === 'development') {
+    //     tkwMeasurementIntervals.push(1);
+    // }
 
     return (
         <VStack w='full' h='full' fontSize={'xs'} p={4} >
