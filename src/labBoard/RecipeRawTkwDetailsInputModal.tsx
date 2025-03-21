cf9f4b3bd95172d936c81731ae42ef400f469f13
Ivan Sliepchenko <ivan.slepchenko@zettech.com>
2025-03-21 22:41:02 +0100

    refactor: improve average TKW calculation to handle division by zero in RecipeRawTkwDetailsInputModal

--- src/labBoard/RecipeRawTkwDetailsInputModal.tsx
+++ src/labBoard/RecipeRawTkwDetailsInputModal.tsx
@@ -57,7 +57,9 @@ const RecipeRawTkwDetailsInputModal: FC<RecipeRawTkwDetailsInputModalProps> = ({
     }, []);
 
     useEffect(() => {
-        setAverageTkw(((tkwRep1 ?? 0) + (tkwRep2 ?? 0) + (tkwRep3 ?? 0)) / 3);
+        let value: number | null = ((tkwRep1 ?? 0) + (tkwRep2 ?? 0) + (tkwRep3 ?? 0)) / ((tkwRep1 ? 1 : 0) + (tkwRep2 ? 1 : 0) + (tkwRep3 ? 1 : 0));
+        if(isNaN(value)) value = null;
+        setAverageTkw(value);
     }, [tkwRep1, tkwRep2, tkwRep3]);
 
     const handleTakeSnapshot = () => {
