f2f268456f467345305b1e3477d4a333fbac3ba3
Ivan Sliepchenko <ivan.slepchenko@zettech.com>
2025-03-25 00:03:01 +0100

    fix: correct Docker image tagging format in deployment script

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ version=$(npm run --silent get-version)
 # Output version to the console
 echo "Deploying version $version"
 # Tag the Docker image
-docker tag protravka-web protravka.azurecr.io/protravka-web:$version
+docker tag protravka-web protravka.azurecr.io/protravka-web:$version:---
 # Push the Docker image to the registry
 docker push protravka.azurecr.io/protravka-web:$version
 # Redeploy the application
