97ef24fe4ed0194a09ff2a3559712d7042c221de
Ivan Sliepchenko <ivan.slepchenko@zettech.com>
2025-03-25 00:00:17 +0100

    chore: update Prettier configuration and clean up deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
-
 # Set the environment to production
 export NODE_ENV=production
-
 # Azure login
 az login
-
 # ACR login
 az acr login --name protravka
-
 # Get the version from package.json with npm
 version=$(npm run --silent get-version)
-
 # Output version to the console
 echo "Deploying version $version"
-
 # Tag the Docker image
 docker tag protravka-web protravka.azurecr.io/protravka-web:$version
-
 # Push the Docker image to the registry
 docker push protravka.azurecr.io/protravka-web:$version
-
 # Redeploy the application
 az containerapp update --name protravka-web --resource-group protravka --image protravka.azurecr.io/protravka-web:$version
